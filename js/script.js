41ec72a6b30ed80e32247663d12cdcd3b5f77362
caSkds <cesar.hidalgo.rdz@comunidad.unam.mx>
2025-11-11 15:30:34 -0600

    init: Creando repo

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const x = 10;
+console.log(x);
+
+const cielo = document.getElementById('cielo');
+
+for(let i = 0; i<5;i++){
+    cielo.innerHTML+='⭐️';
+
+}
+
+const planetas = ['🌍', '🪐', '☀️'];
+
+for (const planeta of planetas){
+    cielo.innerHTML+=planeta;
+}
+
+let n = 0;
+while (n<5){
+    cielo.innerHTML+='🌩️';
+    n++;
+}
+
+let m = 0
+do {
+    cielo.innerHTML+='🚀';
+    m++;
+}while (m<4);
+
+const btnEstrella = document.getElementById('botonEstrella');
+
+btnEstrella.addEventListener('click', ()=>{
+    cielo.innerHTML+='⭐️';
+})
